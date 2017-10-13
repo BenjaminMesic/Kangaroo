09f87283b03fae3903db108478150467986643bd
BenjaminMesic <benjamin.mesic@cern.ch>
2017-10-13 11:22:05 +0200

    f7/firebase scripts added locally. Code reorganised.

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,7 @@
-// here initialize the app
+// ---------------------------------
+// Initialize the app F7
+// ---------------------------------
+
 var myApp = new Framework7();
 
 // If your using custom DOM library, then save it to $$ variable
@@ -9,3 +12,35 @@ var mainView = myApp.addView('.view-main', {
 // enable the dynamic navbar for this view:
 dynamicNavbar: true
 });
+
+// ---------------------------------
+// Phonegap
+// ---------------------------------
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// ---------------------------------
+// Initialize Firebase
+// ---------------------------------
+
+var config = {
+  apiKey: "AIzaSyBs1h4yBkTqq_V89TVtqyFoq0ahDHNXEDg",
+  authDomain: "test-328b5.firebaseapp.com",
+  databaseURL: "https://test-328b5.firebaseio.com",
+  projectId: "test-328b5",
+  storageBucket: "test-328b5.appspot.com",
+  messagingSenderId: "902118517534"
+};
+
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
+
+// ---------------------------------
+// Initialize user
+// ---------------------------------
+var player = {
+  name      : 'mroguljic',
+  longitude : null,
+  latitude  : null,
+  timestamp : null
+}
