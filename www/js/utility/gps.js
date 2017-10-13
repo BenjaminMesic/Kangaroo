66d5be3e16b7d4934c22bf166b046e406ef69f24
BenjaminMesic <benjamin.mesic@cern.ch>
2017-10-13 23:24:39 +0200

    gpsfile path fix

--- www/js/utility/gps.js
+++ www/js/utility/gps.js
@@ -1,48 +1,52 @@
-var grid_division=180
-
-
 var onSuccess = function(position) {
-    updateGPS(position.coords.latitude,position.coords.longitude)
-    alert( 'Longitude: '         + position.coords.longitude         + '\n' +
-           'Latitude: '          + position.coords.latitude          + '\n' +
-/*          'Altitude: '        + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +*/
-          'Timestamp: '         + position.timestamp                + '\n');
+
+  alert('Longitude: '         + position.coords.longitude         + '\n' +
+        'Latitude: '          + position.coords.latitude          + '\n' +
+        'Altitude: '          + position.coords.altitude          + '\n' +
+        'Accuracy: '          + position.coords.accuracy          + '\n' +
+        'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+        'Heading: '           + position.coords.heading           + '\n' +
+        'Speed: '             + position.coords.speed             + '\n' +
+        'Timestamp: '         + position.timestamp                + '\n');
+
+  updateGPS(db, position.coords.latitude, position.coords.longitude, player.name)
 
 };
 
 // onError Callback receives a PositionError object
 //
 function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
-
+  alert('code: '    + error.code    + '\n' +
+        'message: ' + error.message + '\n');
+};
 
 function getGPS(){
-	navigator.geolocation.getCurrentPosition(onSuccess, onError);
-	}
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+};
 
-function getGridPostition(longitude,latitude){
+function updateGPS(database, latitude, longitude, user){
+  var userRef = database.collection("users").doc(user);
+  userRef.update({
+    latitude:latitude,
+    longitude:longitude
+  }).then(function() {
+    console.log("Coordinates successfully updated!");
+  })
+  .catch(function(error) {
+    // The document probably doesn't exist.
+    console.error("Error updating document: ", error);
+  });
+};
+
+// ----------------------------------------------------------
+// Archive
+// ----------------------------------------------------------
+
+var grid_division=180;
+
+function getGridPosition(longitude,latitude){
   x=Math.floor( (longitude/180)*grid_division );
   y=Math.floor( (latitude/90)*grid_division );
   //console.log(x,y);
   return [x,y]
-}
-
-function updateGPS(lat,long,user='mroguljic'){
-  var userRef=db.collection("users").doc(user);
-  userRef.update({
-    latitude:lat,
-    longitude:long
-  }).then(function() {
-    console.log("Coordinates successfully updated!");
-})
-.catch(function(error) {
-    // The document probably doesn't exist.
-    console.error("Error updating document: ", error);
-});
-}
+};
